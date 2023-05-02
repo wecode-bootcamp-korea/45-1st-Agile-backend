c90b1ceecb69815394231c072330003d87544461
Geun9 <gsilverg197@gmail.com>
2023-05-02 14:51:18 +0900

    [ADD]: 초기세팅

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-// const routes = require('./routes');
+const routes = require('./routes');
 
-// const dataSource = require('./models/dataSource');
+const dataSource = require('./models/dataSource');
 
-// dataSource
-//   .initialize()
-//   .then(() => {
-//     console.log('Data Source has been initialized!');
-//   })
-//   .catch((err) => {
-//     console.log('Error occurred during Data Source initialization', err);
-//     dataSource.destroy();
-//   });
+dataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.log('Error occurred during Data Source initialization', err);
+    dataSource.destroy();
+  });
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
