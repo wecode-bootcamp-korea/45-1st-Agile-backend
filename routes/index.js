c6165521995f168ee8746f4cbe894bcf6cd584da
jinss5 <jinss1145@gmail.com>
2023-05-04 15:52:49 +0900

    wip

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const userRouter = require('./userRouter');
 // const bookRouter = require('./bookRouter');
 // const likeRouter = require('./likeRouter');
+const cartRouter = require('./cartRouter');
 
 const router = express.Router();
 
 router.use('/users', userRouter.router);
 // router.use('/books', bookRouter.router);
 // router.use('/likes', likeRouter.router);
+router.use('/carts', cartRouter.router);
 
 module.exports = router;
