f59827c43e29c28a3fd55465a98e250bde9f6bca
jinss5 <jinss1145@gmail.com>
2023-05-05 14:40:15 +0900

    wip

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,9 +2,11 @@ const paymentService = require('../services/paymentService');
 const { catchAsync } = require('../middlewares/error');
 
 const createOrder = catchAsync(async (req, res) => {
-  const quantity = req.body.quantity;
-  const bookId = req.body.bookId;
   const userId = req.user.userId;
+  const { address, quantity, bookId } = req.body;
+
+  await orderService.createOrder(address, userId);
+  await orderService.createOrderItems(quantity, bookId);
 
   return res.status(201).json({ message: 'CREATE SUCCESS' });
 });
