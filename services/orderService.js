ceb457162fb970add2636e758b5ba7952c8494f6
jinss5 <jinss1145@gmail.com>
2023-05-06 22:53:51 +0900

    [modified]:결제잔고부족 오류

--- services/orderService.js
+++ services/orderService.js
@@ -31,6 +31,47 @@ const completeOrder = async (address, userId, bookId, quantity) => {
   return;
 };
 
+const completeOrders = async (address, userId, bookIdAndQuantity) => {
+  let points = 0;
+
+  for (let i = o; i < bookIdAndQuantity.length; i++) {
+    const bookId = bookIdAndQuantity.book_id;
+    const book = await bookDao.getBookById(bookId);
+    const bookPrice = book.price;
+    const quantity = bookIdAndQuantity.quantity;
+
+    const point = bookPrice * quantity;
+
+    points += point;
+  }
+
+  const book = await bookDao.getBookById(bookId);
+
+  const points = book.price * quantity;
+
+  const userPoints = await userDao.getUserById(userId);
+
+  if (userPoints < points) {
+    const error = new Error('INSUFFICIENT_BALANCE');
+    error.statusCode = 404;
+    throw error;
+  }
+
+  await userDao.updateUserPoints(userId, points);
+
+  const orderNumber = uuidv4();
+  const orderStatusId = await orderDao.getOrderStatusId('배송준비중');
+
+  await orderDao.createOrder(orderNumber, address, userId, orderStatusId.id);
+
+  const order = await getOrder(orderNumber);
+  const orderId = order.id;
+
+  await orderDao.createOrderItems(quantity, bookId, orderId);
+
+  return;
+};
+
 const getOrder = async (orderNumber) => {
   return await orderDao.getOrder(orderNumber);
 };
