2fe6d0bc6f0aff1e21c3eb94ef9325a504b9fe0f
Geun9 <gsilverg197@gmail.com>
2023-05-06 22:12:26 +0900

    [MODIFY]: method 변경

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,7 +4,7 @@ const { validateToken } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.delete('', validateToken, cartController.deleteBook);
+router.post('', validateToken, cartController.deleteBook);
 
 module.exports = {
   router,
