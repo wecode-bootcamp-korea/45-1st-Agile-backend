7b87b8671db00c06772b5d6c476100ee8f8cb8f6
jinss5 <jinss1145@gmail.com>
2023-05-07 17:32:00 +0900

    applied transaction

--- services/orderService.js
+++ services/orderService.js
@@ -2,50 +2,71 @@ const orderDao = require('../models/orderDao');
 const bookDao = require('../models/bookDao');
 const userDao = require('../models/userDao');
 const { v4: uuidv4 } = require('uuid');
+const { dataSource } = require('../models/dataSource');
 
 const completeOrders = async (address, userId, bookIdAndQuantity) => {
-  let points = 0;
+  let queryRunner;
 
-  for (let i = 0; i < bookIdAndQuantity.length; i++) {
-    const bookId = bookIdAndQuantity[i].bookId;
+  try {
+    queryRunner = dataSource.createQueryRunner();
 
-    const book = await bookDao.getBookById(bookId);
+    await queryRunner.startTransaction();
 
-    const bookPrice = book.price;
-    const quantity = bookIdAndQuantity[i].quantity;
+    let points = 0;
 
-    const point = bookPrice * quantity;
+    for (let i = 0; i < bookIdAndQuantity.length; i++) {
+      const bookId = bookIdAndQuantity[i].bookId;
 
-    points += point;
-  }
+      const book = await bookDao.getBookById(bookId);
 
-  const user = await userDao.getUserById(userId);
-  const userPoints = user.points;
+      const bookPrice = book.price;
+      const quantity = bookIdAndQuantity[i].quantity;
 
-  if (userPoints < points) {
-    const error = new Error('INSUFFICIENT_BALANCE');
-    error.statusCode = 404;
+      const point = bookPrice * quantity;
+
+      points += point;
+    }
+
+    const user = await userDao.getUserById(userId);
+    const userPoints = user.points;
+
+    if (userPoints < points) {
+      const error = new Error('INSUFFICIENT_BALANCE');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const orderNumber = uuidv4();
+    const orderStatusId = await orderDao.getOrderStatusId('배송준비중');
+
+    await orderDao.createOrder(orderNumber, address, userId, orderStatusId.id);
+
+    for (let i = 0; i < bookIdAndQuantity.length; i++) {
+      const order = await getOrder(orderNumber);
+      const orderId = order.id;
+
+      const quantity = bookIdAndQuantity[i].quantity;
+      const bookId = bookIdAndQuantity[i].bookId;
+
+      await orderDao.createOrderItems(quantity, bookId, orderId);
+    }
+
+    await userDao.updateUserPoints(userId, points);
+
+    await queryRunner.commitTransaction();
+
+    return;
+  } catch (error) {
+    await queryRunner.rollbackTransaction();
+
+    error = new Error('CONNECTION_LOST');
+    error.statusCode = 400;
     throw error;
+  } finally {
+    if (queryRunner) {
+      await queryRunner.release();
+    }
   }
-
-  await userDao.updateUserPoints(userId, points);
-
-  const orderNumber = uuidv4();
-  const orderStatusId = await orderDao.getOrderStatusId('배송준비중');
-
-  await orderDao.createOrder(orderNumber, address, userId, orderStatusId.id);
-
-  for (let i = 0; i < bookIdAndQuantity.length; i++) {
-    const order = await getOrder(orderNumber);
-    const orderId = order.id;
-
-    const quantity = bookIdAndQuantity[i].quantity;
-    const bookId = bookIdAndQuantity[i].bookId;
-
-    await orderDao.createOrderItems(quantity, bookId, orderId);
-  }
-
-  return;
 };
 
 const getOrder = async (orderNumber) => {
